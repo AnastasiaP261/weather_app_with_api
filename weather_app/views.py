56b160d8ce16c0d7f7df234d2be37aa2ca81d18a
anastasia <petrichuk.nastya@yandex.ru>
2021-10-06 00:40:32 +0300

    now data from sites can be downloaded(finished)

--- weather_app/views.py
+++ weather_app/views.py
@@ -14,19 +14,18 @@ from django.urls import reverse
 class ViewWeather(TemplateView):
     template_name = 'get_weather.html'
     model = WeatherData
-# можно было оформить представления в виде классов, но
-# так как в приложении нет страниц как таковых и весь функционал классов все равно
-# не будет использоваться, я решила что это лишнее
+
+    # можно было оформить представления в виде классов, но
+    # так как в приложении нет страниц как таковых и весь функционал классов все равно
+    # не будет использоваться, я решила что это лишнее
     def get(self, request, *args, **kwargs):
         req = self.request.GET
         if req:
-            date_time_of_req = datetime.now()       # для того, чтобы добавить метку времени получения записи
+            date_time_of_req = datetime.now()  # для того, чтобы добавить метку времени получения записи
 
+            # работа с апи сайтов и занесение информации в бд
             self.get_data_from_weatherbit(date_time_of_req)
             self.get_data_from_openweathermap(date_time_of_req)
-
-
-
 
             return HttpResponseRedirect(reverse('weather_app'))
         return render(request, self.template_name, {})
@@ -42,39 +41,41 @@ class ViewWeather(TemplateView):
         }
         result = requests.get(url, params=params)
 
-        for item in json.loads(result.content)['data']:
-            obj = self.model.objects.create(
-                city=item['city_name'],
-                date_time=item['ob_time'],
-                lat=item['lat'],
-                lon=item['lon'],
-                temp=item['temp'],
-                site="weatherbit",
-                date_time_of_req=date_time_of_req,
-            )
-            obj.save()
+        item = json.loads(result.content)['data'][0]
+        print(item)
+        obj = self.model.objects.create(
+            city=item['city_name'],
+            date_time=item['ob_time'],
+            lat=item['lat'],
+            lon=item['lon'],
+            temp=item['temp'],
+            site="weatherbit",
+            date_time_of_req=date_time_of_req,
+        )
+        obj.save()
+
         return result
 
     def get_data_from_openweathermap(self, date_time_of_req):
         url = 'https://api.openweathermap.org/data/2.5/weather'
         params = {
             # 'city_id': 524901,
-            'lon': 37.61556,    # координаты Москвы
-            'lat': 55.4507,     # координаты Москвы
+            'lon': 37.61556,  # координаты Москвы
+            'lat': 55.4507,  # координаты Москвы
             'lang': 'ru',
             'appid': API_KEYS["openweathermap"],
         }
         result = requests.get(url, params=params)
-        print(json.loads(result.content))
-        for item in [json.loads(result.content)]:
-            obj = self.model.objects.create(
-                city=item['name'],
-                # date_time=item['ob_time'],  # на этом сайте нет таймштампа
-                lat=item['coord']['lat'],
-                lon=item['coord']['lon'],
-                temp=item['main']['temp'],
-                site="openweathermap",
-                date_time_of_req=date_time_of_req,
-            )
-            obj.save()
+
+        item = json.loads(result.content)
+        obj = self.model.objects.create(
+            city=item['name'],
+            # date_time=item['ob_time'],  # на этом сайте нет таймштампа
+            lat=item['coord']['lat'],
+            lon=item['coord']['lon'],
+            temp=item['main']['temp'],
+            site="openweathermap",
+            date_time_of_req=date_time_of_req,
+        )
+        obj.save()
         return result
